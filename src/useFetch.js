1e8b7375f7c76d0839457828e24416736329f3f6
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-06-21 00:22:33 +0500

    Display results after pagination logic

--- src/useFetch.js
+++ src/useFetch.js
@@ -10,9 +10,7 @@ export const useFetch = () => {
     const response = await fetch(url);
     const data = await response.json();
 
-    paginate(data);
-
-    setData(data);
+    setData(paginate(data));
     setLoading(false);
   };
 
